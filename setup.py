a87126f49ba13038631710914ed37b64c5f0ee3d
Wade Simmons <wade@wades.im>
2010-05-11 16:11:43 -0600

    bump to 0.2.2, clean up the setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,32 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
 
+trove_classifiers=[
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: BSD License",
+    "License :: DFSG approved",
+    "Intended Audience :: Developers",
+    "Operating System :: OS Independent",
+    "Natural Language :: English",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 2",
+    "Programming Language :: Python :: 2.5",
+    "Programming Language :: Python :: 2.6",
+    "Topic :: Utilities",
+    "Topic :: Software Development :: Libraries",
+    ]
+
 setup(name="ostrich",
-      version="0.2.1",
+      version="0.2.2",
       description="Python port of the Scala Ostrich library",
       author="Wade Simmons",
       author_email="wade@wades.im",
       url="http://github.com/wadey/python-ostrich",
+      py_modules=['ostrich'],
       packages = find_packages(),
+      test_suite="ostrich.test",
       license = "Apache 2.0",
       keywords="ostrich",
       install_requires=['decorator'],
+      classifiers=trove_classifiers,
       zip_safe = True)
