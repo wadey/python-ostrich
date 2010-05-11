a4d5d1bde8a3dc8101a844b62ee5fee4dd556a5c
Wade Simmons <wade@wades.im>
2010-05-11 09:22:08 -0600

    bump version to 0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 
 setup(name="ostrich",
-      version="0.1",
+      version="0.2",
       description="Python port of the Scala Ostrich library",
       author="Wade Simmons",
       author_email="wade@wades.im",
