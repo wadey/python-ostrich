442a519b1c02812898dba92b23a8ab6e059088e4
Wade Simmons <wade@wades.im>
2010-12-13 12:10:56 -0700

    0.2.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ trove_classifiers=[
     ]
 
 setup(name="ostrich",
-      version="0.2.4",
+      version="0.2.5",
       description="Python port of the Scala Ostrich library",
       author="Wade Simmons",
       author_email="wade@wades.im",
